fc2c495f6ac21430b928f9a3c5422f048ed833fc
Gautam <105223230+gautammahar@users.noreply.github.com>
2022-12-28 21:57:48 +0530

    Add files via upload

---
+++ Problems/50_Problems.cpp
@@ -0,0 +1,61 @@
+/*
+Hey i'm Gautam Kumar Mahar 
+Here i solved 50 Practice Problems in c++;
+*/
+ 
+
+//---------------------------------------------------- 
+
+
+
+/*
+Problem 1- 
+Where does a cin stops it extraction of data?
+a) By seeing a blankspace        <---- Right answer
+b) By seeing ()
+c) By seeing newline character
+d) All of the above     
+*/
+
+
+/*
+Problem 2- 
+..............Produce results of typr bool which takes a value true or false ?
+a) Relational expressions    <---- Right answer
+b) Float expressions
+c) Logical expressions
+d) Bitwise expressions 
+*/
+
+
+/*
+Problem 3- 
+The..............is a multiple-branching statement where based on a condition, the control is transferred to one of the many possible points.
+a) while        
+b) For
+c) do-while
+d) switch-case      <---- Right answer
+*/
+
+/*
+Problem 4- 
+Which is the correct syntax to delete a single object?
+a) delete *objectName;  
+b) objectName delete;
+c) delete objectName;         <---- Right answer
+d) objectName *delete;    
+*/
+
+/*
+Problem 4- 
+What is the output of this program;
+int i; 
+cout<<"Please enter an integer value:";
+cin>>i+4;
+cout<<i;
+a) Your input + 4
+b) Your input - 4
+c) Error         <---- Right answer
+d) Garbage value   
+*/
+
